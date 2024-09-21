9603010772ce0e6772d73fc706f5387d3e7f300e
Arnav <arnavchaturvedi5050@gmail.com>
2024-09-21 14:27:58 -0400

    Initial commit

---
+++ src/main/java/net/arnav/caseohmod/CaseOhMod.java
@@ -0,0 +1,18 @@
+package net.arnav.caseohmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CaseOhMod implements ModInitializer {
+	public static final String MOD_ID = "caseoh-mod";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/arnav/caseohmod/CaseOhModClient.java
@@ -0,0 +1,10 @@
+package net.arnav.caseohmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CaseOhModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
